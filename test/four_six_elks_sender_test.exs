88812efd7c67c193f06283721d08aea8bc190414
Mattias Lundberg <mattias@kundo.se>
2018-06-03 19:04:53 +0200

    Cleanup of 46elks test

--- test/four_six_elks_sender_test.exs
+++ test/four_six_elks_sender_test.exs
@@ -10,65 +10,49 @@ defmodule FourSixElksSenderTest do
   import Mock
 
   describe "send_message/1" do
-    test "sends successful response" do
-      assert capture_log(fn ->
-               with_mock HTTPotion,
-                 post: fn _url, _data ->
-                   %HTTPotion.Response{status_code: 200, body: "{\"status\": \"created\"}"}
-                 end do
-                 m = %Message{to: "me", msg: "Warning"}
-                 FourSixElksSender.send_message(m)
-
-                 assert called(
-                          HTTPotion.post(
-                            "https://api.46elks.com/a1/SMS",
-                            body: Message.post_data(m),
-                            ibrowse: [basic_auth: {'testuser', 'secret'}]
-                          )
-                        )
-               end
-             end) =~ "Successfully sent message"
+    test_with_mock "sends successful response", HTTPotion,
+      post: fn _url, _data ->
+        %HTTPotion.Response{status_code: 200, body: "{\"status\": \"created\"}"}
+      end do
+      capture_log(fn ->
+        m = %Message{to: "me", msg: "Warning"}
+        FourSixElksSender.send_message(m)
+      end)
     end
   end
 
   describe "handle_cast/2" do
-    test "successful response" do
-      with_mock HTTPotion,
-        post: fn _url, _data ->
-          %HTTPotion.Response{status_code: 200, body: "{\"status\": \"created\"}"}
-        end do
-        m = %Message{to: "me", msg: "Warning"}
+    test_with_mock "successful response", HTTPotion,
+      post: fn _url, _data ->
+        %HTTPotion.Response{status_code: 200, body: "{\"status\": \"created\"}"}
+      end do
+      m = %Message{to: "me", msg: "Warning"}
 
-        assert capture_log(fn ->
-                 {:noreply, _} = FourSixElksSender.handle_cast(m, nil)
-               end) =~ "Successfully sent message"
-      end
+      assert capture_log(fn ->
+               {:noreply, _} = FourSixElksSender.handle_cast(m, nil)
+             end) =~ "Successfully sent message"
     end
 
-    test "error status from remote" do
-      with_mock HTTPotion,
-        post: fn _url, _data ->
-          %HTTPotion.Response{status_code: 200, body: "{\"status\": \"error\"}"}
-        end do
-        m = %Message{to: "me", msg: "Warning"}
+    test_with_mock "error status from remote", HTTPotion,
+      post: fn _url, _data ->
+        %HTTPotion.Response{status_code: 200, body: "{\"status\": \"error\"}"}
+      end do
+      m = %Message{to: "me", msg: "Warning"}
 
-        assert capture_log(fn ->
-                 {:noreply, _} = FourSixElksSender.handle_cast(m, nil)
-               end) =~ "Failed to send"
-      end
+      assert capture_log(fn ->
+               {:noreply, _} = FourSixElksSender.handle_cast(m, nil)
+             end) =~ "Failed to send"
     end
 
-    test "non 200 response code" do
-      with_mock HTTPotion,
-        post: fn _url, _data ->
-          %HTTPotion.Response{status_code: 500, body: ""}
-        end do
-        m = %Message{to: "me", msg: "Warning"}
+    test_with_mock "non 200 response code", HTTPotion,
+      post: fn _url, _data ->
+        %HTTPotion.Response{status_code: 500, body: ""}
+      end do
+      m = %Message{to: "me", msg: "Warning"}
 
-        assert capture_log(fn ->
-                 {:noreply, _} = FourSixElksSender.handle_cast(m, nil)
-               end) =~ "Failed to send"
-      end
+      assert capture_log(fn ->
+               {:noreply, _} = FourSixElksSender.handle_cast(m, nil)
+             end) =~ "Failed to send"
     end
   end
 end
