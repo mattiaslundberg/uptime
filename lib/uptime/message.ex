618f16349c1b0d584ccc782fc1bd5786fb1398b7
Mattias Lundberg <mattias@kundo.se>
2018-06-03 12:08:39 +0200

    Add post data serialization to message module

--- lib/uptime/message.ex
+++ lib/uptime/message.ex
@@ -1,3 +1,13 @@
 defmodule Uptime.Message do
   defstruct [:to, :msg]
+  @from "Uptime"
+
+  def post_data(%__MODULE__{to: to, msg: msg}) do
+    %{
+      "to" => to,
+      "from" => @from,
+      "message" => msg
+    }
+    |> URI.encode_query()
+  end
 end
---
+++ test/message_test.exs
@@ -0,0 +1,15 @@
+defmodule MessageTest do
+  use ExUnit.Case
+  alias Uptime.Message
+
+  describe "post_data" do
+    test "get encoded data correctly" do
+      m = %Message{
+        to: "+461111111",
+        msg: "Warning: it's down"
+      }
+
+      assert Message.post_data(m) == "from=Uptime&message=Warning%3A+it%27s+down&to=%2B461111111"
+    end
+  end
+end
