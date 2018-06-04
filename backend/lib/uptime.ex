474be8226d742538b7e6545c498bcd4939176230
Mattias Lundberg <mattias@kundo.se>
2018-06-04 16:23:32 +0200

    Nicer start helper

--- backend/lib/uptime.ex
+++ backend/lib/uptime.ex
@@ -1,8 +1,13 @@
 defmodule Uptime do
   alias Uptime.{Checkers, Check}
 
-  def run(url, notify_number) do
-    check = %Check{url: url, notify_number: notify_number}
+  def add_new_check(url, notify_number, expected_code) do
+    check = %Check{
+      url: url,
+      notify_number: notify_number,
+      expected_code: expected_code
+    }
+
     Checkers.add_check(check)
   end
 end
