9c509bfc6de08f2c6e12f9b50e77ce9613ab0075
Mattias Lundberg <mattias@kundo.se>
2018-06-04 13:46:37 +0200

    Add form to add new checks

--- uptime_gui/lib/uptime_gui_web/controllers/page_controller.ex
+++ uptime_gui/lib/uptime_gui_web/controllers/page_controller.ex
@@ -6,9 +6,29 @@ defmodule UptimeGuiWeb.PageController do
 
   def index(conn, _params) do
     checks = Check.get_all()
+    changeset = Check.changeset(%Check{})
 
     conn
+    |> assign(:changeset, changeset)
     |> assign(:checks, checks)
     |> render(:index)
+  end
+
+  def create(conn, _params = %{"check" => check_params}) do
+    checks = Check.get_all()
+    changeset = Check.changeset(%Check{}, check_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _} ->
+        conn
+        |> put_status(:accepted)
+        |> redirect(to: "/")
+
+      {:error, changeset} ->
+        conn
+        |> assign(:changeset, changeset)
+        |> assign(:checks, checks)
+        |> render(:index)
+    end
   end
 end
