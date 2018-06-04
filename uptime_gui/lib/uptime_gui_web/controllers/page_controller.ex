d78d38687e351a4e167f26de11b27a77a9fb84dc
Mattias Lundberg <mattias@kundo.se>
2018-06-04 12:49:27 +0200

    Show list of checks

--- uptime_gui/lib/uptime_gui_web/controllers/page_controller.ex
+++ uptime_gui/lib/uptime_gui_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule UptimeGuiWeb.PageController do
   use UptimeGuiWeb, :controller
 
+  alias UptimeGui.Repo
+  alias UptimeGui.Check
+
   def index(conn, _params) do
-    render conn, "index.html"
+    checks = Check.get_all()
+
+    conn
+    |> assign(:checks, checks)
+    |> render(:index)
   end
 end
