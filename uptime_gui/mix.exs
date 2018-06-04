866979f46427f847de7aace39fe107f4f32620ad
Mattias Lundberg <mattias@kundo.se>
2018-06-04 10:30:21 +0200

    Add mix test.watch

--- uptime_gui/mix.exs
+++ uptime_gui/mix.exs
@@ -41,6 +41,7 @@ defmodule UptimeGui.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
+      {:mix_test_watch, "~> 0.6", only: :dev, runtime: false},
       {:uptime, path: "../backend"}
     ]
   end
