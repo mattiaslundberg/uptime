b1132e91e5653e9915d65d02ee1d1303509ae029
Mattias Lundberg <mattias@kundo.se>
2018-06-04 12:32:46 +0200

    Mix format

--- uptime_gui/lib/uptime_gui_web.ex
+++ uptime_gui/lib/uptime_gui_web.ex
@@ -28,8 +28,9 @@ defmodule UptimeGuiWeb do
 
   def view do
     quote do
-      use Phoenix.View, root: "lib/uptime_gui_web/templates",
-                        namespace: UptimeGuiWeb
+      use Phoenix.View,
+        root: "lib/uptime_gui_web/templates",
+        namespace: UptimeGuiWeb
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
