cbbf9c3e86bfdd9a2887ec83d70aa5db0839a4a8
Mattias Lundberg <mattias@kundo.se>
2018-06-06 09:44:43 +0200

    Cleanup page controller tests

--- uptime_gui/test/uptime_gui_web/controllers/page_controller_test.exs
+++ uptime_gui/test/uptime_gui_web/controllers/page_controller_test.exs
@@ -1,14 +1,6 @@
 defmodule UptimeGuiWeb.PageControllerTest do
   use UptimeGuiWeb.ConnCase, async: false
 
-  alias UptimeGui.{Repo, Check}
-
-  @valid_attrs %{
-    url: "https://example.com",
-    notify_number: "+461234567",
-    expected_code: 200
-  }
-
   describe "index/2" do
     test "get", %{conn: conn} do
       conn = get(conn, "/")
