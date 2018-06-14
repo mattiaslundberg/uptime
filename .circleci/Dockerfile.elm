7a3568917a5385de45a85cf5f313d5db03e9ba12
Mattias Lundberg <mattias@kundo.se>
2018-06-14 19:37:04 +0200

    Compile elm in ci

---
+++ .circleci/Dockerfile.elm
@@ -0,0 +1,28 @@
+FROM ubuntu:latest
+
+RUN apt-get update -y -qq && apt-get install -y -qq git g++ make bash gnupg curl
+RUN git clone https://github.com/obmarg/libsysconfcpus.git &&\
+    cd libsysconfcpus &&\
+    ./configure &&\
+    make && make install
+
+RUN useradd -ms $(which bash) asdf
+
+ENV PATH /home/asdf/.asdf/bin:/home/asdf/.asdf/shims:$PATH
+
+USER asdf
+
+ENV NODEJS_CHECK_SIGNATURES no
+
+RUN /bin/bash -c "git clone https://github.com/asdf-vm/asdf.git ~/.asdf && \
+                  asdf plugin-add nodejs && \
+                  asdf plugin-add elm"
+
+RUN mkdir /home/asdf/repo/
+WORKDIR /home/asdf/repo/
+
+RUN /bin/bash -c "asdf install elm 0.18.0 && asdf install nodejs 10.3.0 && rm -rf /tmp/*"
+
+CMD /bin/bash -c "cd /home/asdf/repo/uptime_gui/assets/elm && \
+                  elm-package install -y && \
+                  elm-make App.elm && cat index.html"
