0e88a32efa367276d68e88e3146d2c0c9ad9d055
Mattias Lundberg <mattias@kundo.se>
2018-06-15 19:10:07 +0200

    Factory support for inserting check owned by user

--- uptime_gui/test/support/factories.ex
+++ uptime_gui/test/support/factories.ex
@@ -1,9 +1,15 @@
 defmodule UptimeGui.Factories do
   alias UptimeGui.{Check, User, Repo}
 
-  def insert_check(opts \\ []) do
+  def insert_check(user \\ nil, opts \\ []) do
+    check =
+      case user do
+        nil -> %Check{}
+        _ -> Ecto.build_assoc(user, :checks)
+      end
+
     Check.changeset(
-      %Check{},
+      check,
       %{
         url: Keyword.get(opts, :url, "https://example.com"),
         notify_number: Keyword.get(opts, :notify_number, "+461234567"),
