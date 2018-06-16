e68a900ec0e428dd204bbf9d045317b8dbbf792a
Mattias Lundberg <mattias@kundo.se>
2018-06-16 12:19:18 +0200

    Add coveralls to gui

--- uptime_gui/mix.exs
+++ uptime_gui/mix.exs
@@ -10,7 +10,14 @@ defmodule UptimeGui.Mixfile do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -41,6 +48,7 @@ defmodule UptimeGui.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
+      {:excoveralls, "~> 0.8", only: :test},
       {:mix_test_watch, "~> 0.6", only: :dev, runtime: false},
       {:argon2_elixir, "~> 1.2"},
       {:cipher, ">= 1.3.4"},
